972e27bde216318ad12a3a088118a01b52848d63
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:34:21 -0300

    feat: create ResponseErrorJson for API error handling

---
+++ src/Shared/UrlShortener.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,19 @@
+﻿namespace CARL.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public IList<string> ErrorMessages { get; private set; }
+    public bool TokenIsExpired { get; set; }
+
+    public ResponseErrorJson(IList<string> errorMessages)
+    {
+        ErrorMessages = errorMessages;
+    }
+
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessages = new List<string> { 
+            errorMessage 
+        };
+    }
+}
