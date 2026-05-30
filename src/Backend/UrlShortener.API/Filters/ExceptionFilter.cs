f2b33ae8adeeea7af04e3c17225df1936374531c
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:42:18 -0300

    feat: add global exception filter for API error handling

---
+++ src/Backend/UrlShortener.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using CARL.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using UrlShortener.Exceptions;
+using UrlShortener.Exceptions.ExceptionsBase;
+
+namespace UrlShortener.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is UrlShortenerException urlShortenerException)
+            HandleProjectException(urlShortenerException, context);
+        else
+            ThrowUnknowException(context);
+    }
+
+    private static void HandleProjectException(UrlShortenerException urlShortenerException, ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)urlShortenerException.GetStatusCode();
+        context.Result = new ObjectResult(new ResponseErrorJson(urlShortenerException.GetErrorMessages()));
+    }
+
+    private static void ThrowUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesException.UNKNOW_ERROR));
+    }
+}
