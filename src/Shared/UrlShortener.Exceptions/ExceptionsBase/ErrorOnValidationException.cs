a54cdb4d38d00257b455e27b56e27c7b4665844b
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:31:31 -0300

    feat: add base exception and validation exception classes

---
+++ src/Shared/UrlShortener.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace UrlShortener.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : UrlShortenerException
+{
+    private readonly IList<string> _errorMessages;
+
+    public ErrorOnValidationException(IList<string> errorMessages) : base(string.Empty)
+    {
+        _errorMessages = errorMessages;
+    }
+
+    public override IList<string> GetErrorMessages()
+    {
+        return _errorMessages;
+    }
+
+    public override HttpStatusCode GetStatusCode()
+    {
+        return HttpStatusCode.BadRequest;
+    }
+}
---
+++ src/Shared/UrlShortener.Exceptions/ExceptionsBase/UrlShortenerException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace UrlShortener.Exceptions.ExceptionsBase;
+
+public abstract class UrlShortenerException : SystemException
+{
+    protected UrlShortenerException(string message) : base(message)
+    {
+
+    }
+
+    public abstract IList<string> GetErrorMessages();
+    public abstract HttpStatusCode GetStatusCode();
+}
