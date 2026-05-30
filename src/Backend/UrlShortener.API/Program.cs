29357fac1a7bf987d22148cff44889d50386c398
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:47:06 -0300

    feat: configure global exception handling

--- src/Backend/UrlShortener.API/Program.cs
+++ src/Backend/UrlShortener.API/Program.cs
@@ -1,4 +1,5 @@
 using CARL.API.Middlewares;
+using UrlShortener.API.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 var app = builder.Build();
 
