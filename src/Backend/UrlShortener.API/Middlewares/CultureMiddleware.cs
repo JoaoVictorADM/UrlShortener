915e80d81f4c517c56d8856265b6e769609da979
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:44:52 -0300

    feat: add culture middleware for request localization

---
+++ src/Backend/UrlShortener.API/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CARL.API.Middlewares;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+        var requestLanguage = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureName = "en"; // Linguagem Padrão da API
+
+        if(!string.IsNullOrWhiteSpace(requestLanguage) && supportedLanguages.Exists(l => l.Name.Equals(requestLanguage)))
+            cultureName = requestLanguage;
+
+        var cultureInfo = new CultureInfo(cultureName);
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
--- src/Backend/UrlShortener.API/Program.cs
+++ src/Backend/UrlShortener.API/Program.cs
@@ -1,3 +1,5 @@
+using CARL.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>(); // Middleware de Cultura para Mensagens Traduzidas
 
 app.UseHttpsRedirection();
 
