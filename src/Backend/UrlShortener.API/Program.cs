b2876b61264c06b07a2175c53722f9c112104025
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:49:28 -0300

    feat: normalize request strings during JSON deserialization

--- src/Backend/UrlShortener.API/Program.cs
+++ src/Backend/UrlShortener.API/Program.cs
@@ -1,3 +1,4 @@
+using CARL.API.Converters;
 using CARL.API.Middlewares;
 using UrlShortener.API.Filters;
 
@@ -5,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new StringConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
