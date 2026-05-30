23b2fd24f9576dd69822dffd3850b83ec11a0d61
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 14:54:13 -0300

    feat: add Url domain entity

---
+++ src/Backend/UrlShortener.Domain/Entities/Url.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Domain.Entities;
+
+public class Url
+{
+    public string ShortCode { get; set; } = string.Empty;
+    public string OriginalUrl { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
