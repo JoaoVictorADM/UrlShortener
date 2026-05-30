9b71126a4b1b5dd36e178a5e86694dc02854cce2
Joao <joaomeira@alunos.utfpr.edu.br>
2026-05-30 15:01:26 -0300

    feat: implement unit of work pattern

---
+++ src/Backend/UrlShortener.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace UrlShortener.Domain.Repositories;
+
+public interface IUnitOfWork
+{
+    public Task Commit();
+}
---
+++ src/Backend/UrlShortener.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using UrlShortener.Domain.Repositories;
+
+namespace UrlShortener.Infrastructure.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly UrlShortenerDbContext _dbContext;
+
+    public UnitOfWork(UrlShortenerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Commit()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
